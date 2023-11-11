99f12c60c03fda9ea5e14697850e66670ae1aed5
Sebastian Almiron <sebastianr_almiron@hotmail.com>
2023-11-11 15:36:31 -0300

    cambios

--- js/commons.js
+++ js/commons.js
@@ -1,17 +1,18 @@
 let data;
+const url = 'https://resultados.mininterior.gob.ar/api/'
 let selectFecha = document.getElementById('filtro-year');
 let selectCargo = document.getElementById('filtro-cargo');
 let selectDistrito = document.getElementById('filtro-distrito');
 
 let anioEleccion;
-let tipoRecuento;
-let tipoEleccion;
+let tipoRecuento = "1";
+let tipoEleccion = "1";
 let cargoEleccion;
 let distritoEleccion;
 
 async function periodos() {
     try {
-        let response = await fetch('https://resultados.mininterior.gob.ar/api/menu/periodos');
+        let response = await fetch(`${url}menu/periodos`);
         data = await response.json();
         let optionFecha;
         for (let i = 0; i < data.length; i++) {
@@ -23,14 +24,15 @@ async function periodos() {
         console.error('Error en respuesta: ' + error);
     }
 }
+let c = false;
 
 periodos();
 let anioSeleccionado = false;
 
 selectFecha.addEventListener('change', function() {
     // Borra las opciones anteriores de selectCargo y selectDistrito
-    selectCargo.innerHTML = '';
-    selectDistrito.innerHTML = '';
+    selectCargo.innerHTML = '<option disable selected hidden>Cargo</option>';
+    selectDistrito.innerHTML = '<option disable selected hidden>Distrito</option><option>Seleccione un cargo</option>';
     
     anioEleccion = selectFecha.value;
     anioSeleccionado = true;
@@ -41,7 +43,7 @@ selectFecha.addEventListener('change', function() {
 
 selectCargo.addEventListener('change', function() {
     // Borra las opciones anteriores de selectDistrito
-    selectDistrito.innerHTML = '';
+    selectDistrito.innerHTML = '<option disable selected hidden>Distrito</option>';
     
     anioEleccion = selectFecha.value;
     cargoEleccion = selectCargo.value;
@@ -61,11 +63,12 @@ selectDistrito.addEventListener('change', function() {
     cargarOpciones(anioEleccion, 2, cargoEleccion, distritoEleccion);
 });
 
+let v = false;
 Filtrar()
 
 async function cargarOpciones(anioEleccion, idEleccion, cargoEleccion = null) {
     try {
-        let response = await fetch(`https://resultados.mininterior.gob.ar/api/menu?año=${anioEleccion}`);
+        let response = await fetch(`${url}menu?año=${anioEleccion}`);
         let data = await response.json();
         
         for (let i = 0; i < data.length; i++) {
@@ -79,17 +82,18 @@ async function cargarOpciones(anioEleccion, idEleccion, cargoEleccion = null) {
                     optionCargo.value = data[i].Cargos[j].IdCargo;
                     selectCargo.appendChild(optionCargo);
                     
-                    if (cargoEleccion && data[i].Cargos[j].Cargo === cargoEleccion) {
+                    if (cargoEleccion && data[i].Cargos[j].IdCargo === cargoEleccion) {
                         
                         const distritos = data[i].Cargos[j].Distritos; //JSON
                         
                         for (let k = 0; k < distritos.length; k++) {
                             
                             let optionDistrito = document.createElement('option');
-                            optionDistrito.value = distritos[k].Distrito;//TOODS LOS DISTRITOS
                             optionDistrito.textContent = distritos[k].Distrito;
+                            optionDistrito.value = distritos[k].IdDistrito;//TOODS LOS DISTRITOS
                             selectDistrito.appendChild(optionDistrito);
                             distritoEleccion = selectDistrito.value
+                            v = true;
                             
                         }
                     }
@@ -98,25 +102,34 @@ async function cargarOpciones(anioEleccion, idEleccion, cargoEleccion = null) {
         }
     } catch (error) {
         console.error('Error en respuesta: ' + error);
+        if (error instanceof Response) {
+            console.error('Código de estado:', error.status);}
     }
 }
 
 
 async function Filtrar() {
-    console.log(anioEleccion)
-    console.log(distritoEleccion)
-    console.log(cargoEleccion)
-    console.log(tipoRecuento)
-    console.log(tipoEleccion)
     
     try {
-        const url = `https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=${anioEleccion}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${cargoEleccion}&distritoId=${distritoEleccion}`
+        if (v == true){
+            console.log(anioEleccion)
+            console.log(tipoRecuento)
+            console.log(tipoEleccion)
+            console.log(cargoEleccion)
+            console.log(distritoEleccion)
+
+            const url = 'https://resultados.mininterior.gob.ar/api/resultados/getResultados'
+            let response = await fetch(`https://resultados.mininterior.gob.ar/api/resultados/getResultados?anioEleccion=${anioEleccion}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${cargoEleccion}&distritoId=${distritoEleccion}`);
+            let resultados = await response.json();
         
-        let response = await fetch(url);
-        let resultados = await response.json();
-        
-        console.log(resultados)
+            console.log(resultados)
+        }
+
     } catch (error) {
         console.error('Error en respuesta: ' + error);
+        if (error instanceof Response) {
+            console.error('Código de estado:', error.status);
+            console.error('Mensaje de estado:', error.statusText);
+        }
     }
 }
